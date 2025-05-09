5040486bd27c6cf8571dd54498545bc95a50609a
AshEvans <ashhhevanss@gmail.com>
2025-05-09 14:49:21 +0100

    Update build.sh with correct Playwright dependencies for Ubuntu

--- build.sh
+++ build.sh
@@ -5,8 +5,12 @@ python -m pip install --upgrade pip
 pip install -r requirements.txt
 # Install Playwright
 pip install playwright
-# Install system dependencies for Playwright
+# Add additional apt sources for newer packages
 apt-get update
+apt-get install -y software-properties-common
+add-apt-repository -y ppa:ubuntu-toolchain-r/test
+apt-get update
+# Install system dependencies for Playwright
 apt-get install -y \
   libglib2.0-0 \
   libnss3 \
@@ -25,14 +29,18 @@ apt-get install -y \
   libpango-1.0-0 \
   libcairo2 \
   libsoup-3.0-0 \
+  libgstreamer1.0-0 \
   libgstreamer-gl1.0-0 \
-  libgstreamer-plugins-bad1.0-0 \
+  gstreamer1.0-plugins-bad \
   libenchant-2-2 \
   libsecret-1-0 \
   libmanette-0.2-0 \
-  libgles2 \
-  libwebkit2gtk-4.0-37 \
+  libgles2-mesa \
+  libwebkit2gtk-4.1-0 \
   libgtk-3-0 \
-  libegl1
+  libegl1-mesa \
+  libwoff1 \
+  libharfbuzz-icu0 \
+  libgstreamer-plugins-base1.0-0
 # Install Playwright browsers
 playwright install
